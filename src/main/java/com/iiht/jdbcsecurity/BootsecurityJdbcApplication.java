4670c0d07b7347a2bb716e703dcb5b13b471e95f
ebthisam <75974295+ebthisam@users.noreply.github.com>
2024-07-30 13:00:45 +0530

    Add files via upload

---
+++ src/main/java/com/iiht/jdbcsecurity/BootsecurityJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.iiht.jdbcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootsecurityJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootsecurityJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iiht/jdbcsecurity/BootsecurityJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiht.jdbcsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootsecurityJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
